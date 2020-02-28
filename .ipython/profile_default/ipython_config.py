d058f01c9bbdfb6ab28b56389c152b69856c6f5b
AleCandido <candido.ale@gmail.com>
2020-02-28 11:49:24 +0100

    ipython -> vi mode (2)

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -293,7 +293,7 @@
 # c.TerminalInteractiveShell.display_completions = 'multicolumn'
 
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
-# c.TerminalInteractiveShell.editing_mode = 'emacs'
+c.TerminalInteractiveShell.editing_mode = "vim"
 
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
 c.TerminalInteractiveShell.editor = "vim"
