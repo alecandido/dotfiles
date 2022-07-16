a176dce734724621dac2d680af6f2086064f3746
Alessandro Candido <candido.ale@gmail.com>
2022-07-16 19:49:57 +0200

    It's Working!

--- .config/sh/app.aliases.sh
+++ .config/sh/app.aliases.sh
@@ -82,8 +82,8 @@ mkpdf() {
 # │ server │
 # └────────┘
 
-alias fbcave="ssh phantomblot 'nohup filebrowser -r cave -p 9001 > /dev/null &'"
-alias tunnel-fb="fbcave && ssh -L 9001:localhost:9001 -N -T phantomblot &"
+alias fbcave="filebrowser -r cave -p 9001"
+alias tunnel-fb="ssh -L 9001:localhost:9001 -N -T phantomblot &"
 
 # ┌─────┐
 # │ Fun │
