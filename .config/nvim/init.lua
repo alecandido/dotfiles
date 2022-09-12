ae3f18c5401dc1ec29046986c76cf7b45add0fda
Alessandro Candido <candido.ale@gmail.com>
2022-09-12 17:26:19 +0200

    better code

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- leader and local leader
+vim.g.mapleader = ",,"
+vim.g.maplocalleader = "\\"
+
+-- increase memory for patter matching
+vim.opt.maxmempattern = 10000
+
+-- shada: 
+--      - increase file tracked for markers
+--      - enable file marks
+--      - increase line saved per register
+vim.opt.shada = "'1000,f1,<500"
+
+vim.cmd("source $XDG_CONFIG_HOME/nvim/plug.vim")
